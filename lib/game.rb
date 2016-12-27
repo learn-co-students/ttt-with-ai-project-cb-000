4b8f9053919322556b18cebf06b1e150efa7d3f6
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2016-12-27 06:28:30 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,70 +1,95 @@
 class Game
-   attr_accessor :board, :player_1, :player_2
-   WIN_COMBINATIONS = [
-     [0,1,2],
-     [3,4,5],
-     [6,7,8],
-     [0,3,6],
-     [1,4,7],
-     [2,5,8],
-     [0,4,8],
-     [6,4,2]
-   ]
+  attr_accessor :board, :player_1, :player_2
 
-   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-     @board = board
-     @player_1 = player_1
-     @player_2 = player_2
-   end
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
 
-   def over?
-     won? || draw?
-   end
+  def current_player
+    if self.board.turn_count % 2 == 0
+      @player_1
+    else
+      @player_2
+    end
+  end
 
-   def current_player
-     @board.turn_count % 2 == 0 ? @player_1 : @player_2
-   end
+  def over?
+    if self.board.full? || won?
+      true
+    else
+      false
+    end
+  end
 
-   def winner
-     if winning_combo = won?
-       @winner = @board.cells[winning_combo.first]
-     end
-   end
+  def won?
+    if self.combination_test != nil
+      true
+    else
+      false
+    end
+  end
 
- def turn
-     player = current_player
-     current_move = player.move(@board)
-     if !@board.valid_move?(current_move)
-       turn
-     else
-       puts "Turn: #{@board.turn_count+1}\n"
-       @board.display
-       @board.update(current_move, player)
-       puts "#{player.token} moved #{current_move}"
-       @board.display
-       puts "\n\n"
-     end
-   end
+  def combination_test
+    result = nil
+    WIN_COMBINATIONS.each do |win_combination|
 
-   def play
-     while !over?
-       turn
-     end
-     if won?
-       puts "Congratulations #{winner}!"
-     elsif draw?
-       puts "Cat's Game!"
-     end
-   end
+      position_1 = self.board.cells[win_combination[0]]
+      position_2 = self.board.cells[win_combination[1]]
+      position_3 = self.board.cells[win_combination[2]]
 
-   def won?
-     WIN_COMBINATIONS.detect do |combo|
-       @board.cells[combo[0]] == @board.cells[combo[1]] &&
-       @board.cells[combo[1]] == @board.cells[combo[2]] &&
-       @board.taken?(combo[0]+1)
-     end
-   end
-    def draw?
-     @board.full? && !won?
-   end
- end
+      if position_1 != " " && position_1 == position_2 && position_1 == position_3
+        result = win_combination
+      end
+    end
+    result
+  end
+
+  def draw?
+    if self.over? && !self.won?
+      true
+    else
+      false
+    end
+  end
+
+  def winner
+    if won?
+      player = combination_test
+      return self.board.cells[player[0]]
+    else
+      return nil
+    end
+  end
+
+  def turn
+    input = self.current_player.move(self.board)
+    while !self.board.valid_move?(input)
+      input = self.current_player.move(self.board)
+    end
+    self.board.update(input, self.current_player)
+  end
+
+  def play
+    until over? || won? || draw? do
+      turn
+    end
+    if won?
+      puts "Congratulations #{self.winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+end
