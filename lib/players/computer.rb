137a0960670b8cb2ecf17e906cb0fe24853a5818
John Knotts <jhnnyk@gmail.com>
2017-02-24 07:18:10 -0700

    computer can block win

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,14 @@
+require 'pry'
+
 module Players
   class Computer < Human
     
     def move(board)
       choice = nil
 
-      if board.valid_move?("5")
+      if self.prevent_win(board)
+        choice = self.prevent_win(board)
+      elsif board.valid_move?("5")
         choice = "5" #choose the middle if it's open
       else
         #choose a random number
@@ -16,6 +20,22 @@ module Players
 
       puts "Computer chooses #{choice}"
       choice
+    end
+
+    def prevent_win(board)
+      opponent_token = ""
+      self.token == "X" ? opponent_token = "O" : opponent_token = "X"
+
+      line_to_block = Game::WIN_COMBINATIONS.detect do |line|
+        line.count{|line_index| board.cells[line_index] == opponent_token} == 2
+      end
+
+      if line_to_block
+        index = line_to_block.select {|i| board.cells[i] == " "}
+        if index != []
+          choice = (index[0] + 1).to_s
+        end
+      end
     end
 
   end
