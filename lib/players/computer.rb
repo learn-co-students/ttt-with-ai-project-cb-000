950ccbff788b974087c329f09af48a256f5f9af7
David Ordovskiy <david.ordovsky@gmail.com>
2017-02-25 09:08:17 -0500

    add some comments, assignment done

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,13 @@
 module Players
 	class Computer < Player
 
+		# minimax was supposed to be here, but hasn't showed up :)
+
+		# for now, computer looks at the center and the top right corner
+		# and either places there or selects an available space randomly.
+
+		# any help with implementing minimax here would be strongly appreciated!
+
 		def move(board)
 			potential = board.cells.dup
  			return '5' if potential[4] == ' '
