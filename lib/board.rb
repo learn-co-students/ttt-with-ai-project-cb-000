07e8a707c7a2b75ea5f6f6cd258ccfdb11286795
tahbristol <tahbristol@gmail.com>
2017-04-07 16:57:36 -0400

    Board class plus reset! and display

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,16 @@
-board = ["","","","","","","","",""]
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+
+
+  def reset!
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+
+  def display
+    puts 
+  end
+end
