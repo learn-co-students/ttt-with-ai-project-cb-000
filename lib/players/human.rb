8a9e0278c1a6a631450e9f1927a7aa4f340ebd9b
tahbristol <tahbristol@gmail.com>
2017-04-09 00:33:21 -0400

    human class and part of game class

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,18 @@
+require_relative '../player.rb'
+module Players
 
-class Human < Player
+  class Human < Player
+
+    def move(input)
+      puts "Where would like to move?"
+      input = gets.strip
+    end
 
 
 
 
+
+
+  end
 
 end
