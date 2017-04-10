a5caa04b38f2439dd74b912029c1ebeca1b2cfe0
tahbristol <tahbristol@gmail.com>
2017-04-10 00:48:09 -0400

    added default initialization for players and board

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,21 @@
+require_relative '.\players\human.rb'
+
 class Game
   WIN_COMBINATIONS =  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
   attr_accessor :board, :player_1, :player_2
 
-  def board(player_1, player_2, board)
-   board
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
 
+  def board
+    @board
+  end
+
+  def current_player
+    
   end
 end
