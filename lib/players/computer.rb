49139cbe9e07cf4fa4d0bdbb1be0ed25ff4831d3
Marcus Havranek <marcus.havranek@havranek.co.at>
2017-09-01 13:00:58 +0200

    added some strategy to the ai player

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,14 +2,26 @@ module Players
   class Computer < Player
 
     def move(board)
-      # AI should look at the board and check if move is valid, if move is able to win
-      valid_moves = []
-      (1..9).each do |pos|
-        valid_moves << "#{pos}" if board.valid_move?(pos)
-      end
-      pick = Random.rand(0...valid_moves.length)
-      valid_moves[pick]
-    end
+      # decide on some strategy depending on starting own starting position (am I first or second player)
+      # see https://www.quora.com/Is-there-a-way-to-never-lose-at-Tic-Tac-Toe
+      move = nil
+      # take the middle if starting, if second take it if still free
 
+      if !board.taken?(5)
+        move = "5"
+      elsif board.turn_count == 1 # player is second
+        move = "7"
+      elsif board.turn_count ==
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+
+      else
+        valid_moves = []
+        (1..9).each do |pos|
+          valid_moves << "#{pos}" if board.valid_move?(pos)
+        end
+        pick = Random.rand(0...valid_moves.length)
+        valid_moves[pick]
+      end
+    end
   end
 end
