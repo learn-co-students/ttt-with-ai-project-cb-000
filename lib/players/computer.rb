24338800ce32f955b615d5dbd87ce06fe884228b
Uzor <uubabukoh@gmail.com>
2017-10-14 14:26:28 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,18 @@ module Players
   class Computer < Player
 
     def move(board)
-    "1"
+
+      chosen = 0
+      for i in 1..9
+        pos = i.to_s
+        if board.valid_move?(pos)
+          chosen = i
+          break
+        end
+      end
+      chosen.to_s
+
+
     end
 
   end
