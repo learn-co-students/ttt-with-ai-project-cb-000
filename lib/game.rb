9e5aefcf12636630682222b1426abd3a06ed9881
Matija Simin <marcussimon12082015@hotmail.com>
2017-11-13 21:47:42 +0100

    Adding basic validation

--- lib/game.rb
+++ lib/game.rb
@@ -58,22 +58,29 @@ class Game
       puts "2 - human vs human"
       puts "Type exit to quit!"
       type_of_game = gets.strip
-
-      break if type_of_game == "exit"
-      game = Game.new(Players::Computer.new("X"),Players::Computer.new("O")) if type_of_game == "0"
-      if type_of_game == "1"
-        puts "Who should go first? 1 - Computer |-|-| 2 - Human?"
-            go_first = gets.strip
-            if go_first == "1"
-              game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
-            else
-              game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
-            end
+      if  ["exit","0","1","2"].detect{|element| element == type_of_game} != nil
+        break if type_of_game == "exit"
+        game = Game.new(Players::Computer.new("X"),Players::Computer.new("O")) if type_of_game == "0"
+		  if type_of_game == "1"
+			loop do
+			end_loop = false
+			puts "Who should go first? 1 - Computer |-|-| 2 - Human?"
+				go_first = gets.strip
+				if ["1","2"].detect{|element| element == go_first} != nil
+				  if go_first == "1"
+					game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
+				  else
+					game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
+				  end
+				  end_loop = true
+				end
+				break if end_loop
+			  end
+		  end
+		  game = Game.new if type_of_game == "2"
+		  game.play
       end
-      game = Game.new if type_of_game == "2"
-      game.play
     end
   end
-
 
 end
