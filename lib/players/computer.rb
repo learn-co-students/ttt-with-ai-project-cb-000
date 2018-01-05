c193b1c4787bf6918e5cb33baa02e8142d5e0f50
David Kuruvilla <dapawn@gmail.com>
2018-01-05 20:31:33 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,35 +1,70 @@
+require "pry"
 module Players
   class Computer < Player
-    def move(board)
-      move = nil
 
-      # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
-      if !board.taken?(5)
-        move = "5"
-      # If going second and the middle square is taken, take the upper-left corner square.
-      elsif board.turn_count == 1
-        move = "1"
-      # If you went first (and took the middle), take a corner square with your second move.
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-      # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
-      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
-        move = "2"
-      # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
-      else
-        Game::WIN_COMBINATIONS.detect do |cmb|
-          # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
-          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          # If you can't play any winning moves, play a move to block the opponent from winning.
-          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          end
-        end
-        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
-        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
-      end
-      move
+    WIN_COMBINATIONS = [
+    [0,1,2],  # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+    def initialize(token)
+      super
+      @opponent = @token == "X" ? "O" : "X" #Set Opposite
     end
+
+    def move(board)
+      if my_move = win_move?(board,@token) #Take te win
+        (my_move+1).to_s
+      elsif my_move = win_move?(board,@opponent) #Block opponent's win
+        (my_move+1).to_s
+      elsif my_move = trap_move?(board,@token,@opponent) #Setup a 2way trap
+        (my_move+1).to_s
+      elsif my_move = trap_move?(board,@opponent,@token) #Block opponent's possible 2way trap
+        (my_move+1).to_s
+      elsif my_move = setup_move?(board,@opponent) #Try for a 2way trap
+        (my_move+1).to_s
+      else
+        valid_moves(board).map{|e| e+1}.sample.to_s #Convert to Human understandable positions
+      end
+    end
+
+    def valid_moves(board) #Returns Array indexes of valid moves
+      #binding.pry
+      board.cells.collect.with_index {|c, i|  i if c == " "}.compact
+    end
+
+    def win_move?(board, token)
+      temp_cells = board.cells
+      valid_moves(board).detect do |indx| #See if we can find a move that will win for token
+        temp_cells[indx] = "#{token}"
+        win = WIN_COMBINATIONS.detect {|line| line.all? {|i| temp_cells[i] == token}; }
+        temp_cells[indx] = " "
+        win
+      end
+    end
+
+    def trap_move?(board, token, opponent)
+      temp_cells = board.cells
+      valid_moves(board).detect do |indx| #See if we can find a 2way trap move for token
+        #select win lines that include indx, then select those that don't have opponent token, from those select those with our token. If there's two, should set a trap.
+        WIN_COMBINATIONS.select{|c| c.include?(indx)}.select{|line| line.none?{|i| temp_cells[i] == opponent}}.select{|line| line.detect{|i| temp_cells[i] == token}}.count > 1
+      end
+      #binding.pry
+    end
+
+    def setup_move?(board, opponent)
+      temp_cells = board.cells
+      valid_moves(board).detect do |indx| #See if we can find a 2way trap move for token
+        #select win lines that include indx, then select those that don't have opponent token, from those select those with our token. If there's two, should set a trap.
+        WIN_COMBINATIONS.select{|c| c.include?(indx)}.select{|line| line.none?{|i| temp_cells[i] == opponent}}.count > 1
+      end
+    end
+
   end
 end
