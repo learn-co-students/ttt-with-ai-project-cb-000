2efeb6284c97be0df967d4ca290bec1c83ab4d2b
Jen Poole <contact.vandepool@gmail.com>
2018-02-11 22:54:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,9 @@
 # Players::Computer inherits from Player
 module Players
   class Computer < Player
+    # Players::Computer #move returns a valid position for the computer to move
+    def move(board)
+      
+    end
   end
 end
-
-# Players::Computer #move returns a valid position for the computer to move
