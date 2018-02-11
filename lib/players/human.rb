32e7c16e04e2b8f737c6fec6fa1ca426f9820239
Jen Poole <contact.vandepool@gmail.com>
2018-02-11 22:28:33 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,10 @@
 # Players::Human inherits from Player
 module Players
   class Human < Player
+    # Players::Human #move asks the user for input and returns it
+    def move(board)
+      puts "Please enter 1-9:"
+      gets.strip
+    end
   end
 end
-
-# Players::Human #move asks the user for input and returns it
