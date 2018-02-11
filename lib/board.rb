326f342abd138ca96b7e23e0f7447fc8e4ec16af
Jen Poole <contact.vandepool@gmail.com>
2018-02-11 22:11:26 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -13,14 +13,18 @@ class Board
   def reset!
     @cells = Array.new(9, " ")
   end
+
+  # Board #display prints the board
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
 end
 
 
-
-
-
-
-# Board #display prints the board
 
 # Board #position takes in user input and returns the value of the board cell
 
