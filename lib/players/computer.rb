60971e57ce8833c677fdd30bf45353996ef893aa
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-22 15:25:41 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,95 +1,16 @@
 module Players
   class Computer < Player
-    @counter = 0
+
     def move(board)
-      @counter = counter
       @board = board
       @win_combos = Game::WIN_COMBINATIONS
       @opponent = self.token == "X" ? "O" : "X"
-      puts "Thinking..."
-      sleep 2
-=begin
-      if counter <= 1
-        if @board.cells[4] != " "
-          5
-        else
-          1 if
-
-      end
-      counter =+ 1
-=end
+      #puts "Thinking..."
+      #sleep 2
       make_move
     end
-=begin
-    def make_move
-      move = nil
-      @win_combos.find do |combo|
-        if @board.cells[combo[0]] != " " && @board.cells[combo[1]] != " " && !@board.taken?(combo[2]+1)
-          move = @board.taken?(combo[2]+1)
-        end
-      end
-      move
-    end
 
 
-=end
-
-
-
-
-
-
-
-=begin
-     def whats_left
-      @board.cells.each do |cell|
-        if cell
-          self.remaining << cell
-        end
-
-    end
-=end
-=begin
-    def make_move
-      result = nil
-      @win_combos.each do |array|
-        array.find do |combo|
-          if @board.cells[array[0]] == combo
-          result = !@board.taken?(0..8)
-        else
-          result = !@board.taken?(0..8)
-        end
-      end
-      end
-      result
-
-    end
-=end
-=begin
-    def make_move
-        move = nil
-      if !@board.taken?(5)
-        5
-      else
-
-        @win_combos.each do |array|
-          array.find do |combo|
-
-          if @board.cells[combo[0]] != " " &&  @board.cells[combo[1]] != " " && !@board.taken?(combo[2])
-            move = combo[2]
-          elsif  @board.cells[combo[1]] != " " &&  @board.cells[combo[2]] != " " && !@board.taken?(combo[0])
-            move = combo[0]
-          elsif  @board.cells[combo[0]] != " " &&  @board.cells[combo[2]] != " " && !@board.taken?(combo[1])
-            move = combo[1]
-          end
-          end
-          move
-        end
-      end
-    end
-=end
-
-=begin
     def make_move
       case
         when !@board.taken?(5)
@@ -114,7 +35,7 @@ module Players
           6
       end
     end
-=end
+
   end
 
 end
