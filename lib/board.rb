e8f5d41b9abd7699d9a43fb4a26d4d0fc552284a
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-08-10 02:18:05 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -2,17 +2,18 @@ class Board
   attr_accessor :cells
   def initialize 
    @cells = [" ", " ", " ", " ", " ", " ", " ", " "," "]
- end
+  end
  
- def reset!
-   @cells.clear
-   @cells = [" ", " ", " ", " ", " ", " ", " ", " "," "]
- end
+  def reset!
+    @cells.clear
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " "," "]
+  end
  
- def display 
-   puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-   puts "-----------"
-  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-   puts "-----------"
- end
+  def display 
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
 end
