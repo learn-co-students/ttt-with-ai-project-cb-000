0a32103f7786cab949eb3a84ffd1535c9e7a02bc
marysue <mary@marylark.com>
2019-03-21 14:01:01 -0700

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,82 +1,85 @@
 class Board
-    OPEN = " "
+  OPEN = " "
 
-    attr_accessor :cells
+  attr_accessor :cells
 
-    def initialize
-        @cells=[]
-        for i in 0 .. 8 do
-            @cells << OPEN
-        end
+  def initialize
+      @cells=[]
+      for i in 0 .. 8 do
+          @cells << OPEN
+      end
 
-    end
+  end
 
-    def availCells
-        avail = []
-        @cells.each_index {|x| @cells[x] == OPEN ? avail << x : false}
-        avail          
-        
+  def availCells
+      avail = []
+      @cells.each_index {|x| @cells[x] == OPEN ? avail << x : false}
+      avail          
+      
 
-    end
+  end
 
-    def reset!
-        #clears cells
-        for i in 0 .. 8 do
-            @cells[i] = OPEN
-        end
-        @cells
-    end
+  def reset!
+      #clears cells
+      for i in 0 .. 8 do
+          @cells[i] = OPEN
+      end
+      @cells
+  end
 
-    def display(cells = @cells)
-        puts("")
-        puts(" #{cells[0]} | #{cells[1]} | #{cells[2]} \n")
-        puts("-----------")
-        puts(" #{cells[3]} | #{cells[4]} | #{cells[5]} \n")
-        puts("-----------")
-        puts(" #{cells[6]} | #{cells[7]} | #{cells[8]} \n")
-        puts("")
+  def display(cells = @cells)
+      puts("")
+      puts(" #{cells[0]} | #{cells[1]} | #{cells[2]} \n")
+      puts("-----------")
+      puts(" #{cells[3]} | #{cells[4]} | #{cells[5]} \n")
+      puts("-----------")
+      puts(" #{cells[6]} | #{cells[7]} | #{cells[8]} \n")
+      puts("")
 
-    end
+  end
 
-    def position(pos)
-        #pos is a string, convert to int
-        @cells[pos.to_i-1]
-    end
+  def position(pos)
+      #pos is a string, convert to int
+      @cells[pos.to_i-1]
+  end
 
 
-    def update(pos, playerObj)
-        #updates cell with playerToken
-        puts("Updating board for player #{playerObj.token} for cell #{pos.to_i}")
-        @cells[pos.to_i - 1] = playerObj.token
-    end
+  def update(pos, playerObj)
+      #updates cell with playerToken
+      #input MUST be open cell, or current token
+      #will be overwritten
+      puts("Updating board for player #{playerObj.token} for cell #{pos.to_i}")
+      @cells[pos.to_i - 1] = playerObj.token
+  end
 
-    def full?
-        #boolean returning whether board is full
-        retval = true
-        @cells.each{ |c| retval = false unless c != OPEN }
-        retval
- 
-    end
-    def turn_count
-        #returns number of positions filled on the board
-        i = 0
-        @cells.each { |c| i += 1 unless c == OPEN }
-        i
-    end
+  def full?
+      #boolean returning whether board is full
+      retval = true
+      @cells.each{ |c| retval = false unless c != OPEN }
+      retval
 
-    def valid_move?(pos)
-        #returns true if position is empty
-        #validate string is digits
-        if pos.scan(/\D/).empty? && pos.to_i <=9 && pos.to_i >=1
-           # @cells[pos.to_i - 1] == OPEN ? true : false
-           true
-        else
-            false
-        end
-    end
+  end
+  def turn_count
+      #returns number of positions filled on the board
+      i = 0
+      @cells.each { |c| i += 1 unless c == OPEN }
+      i
+  end
 
-    def taken?(pos)
-        @cells[pos.to_i - 1] == OPEN ? false : true
-    end
+  def valid_move?(pos)
+      #returns true if position chosen is 0-8 and 
+      #string contains no characters (only digits)
+      #does NOT validate if cell chosen is empty
+      #see taken?
+      if pos.scan(/\D/).empty? && pos.to_i <=9 && pos.to_i >=1 && !taken?(pos.to_i)
+         true
+      else
+          false
+      end
+  end
+
+  def taken?(pos)
+      @cells[pos.to_i - 1] == OPEN ? false : true
+  end
 
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,20 +1,20 @@
 module Players
-    class Human < Player
-        def move(board)
-            input = 0
-            loop do
-                print("Player '#{@token}' square [1-9]: ")
-                input = gets.strip
-                puts("")
-                if !board.valid_move?(input)
-                    puts("Selection must be between 1-9.")
-                elsif board.taken?(input)
-                    puts("Position taken.")
-                else
-                    break;
-                end #if
-            end #loop
-            input
-        end #move
-    end #Human
+  class Human < Player
+      def move(board)
+          input = 0
+          loop do
+              print("Player '#{@token}' choose square [1-9]: ")
+              input = gets.strip
+              puts("")
+              if !board.valid_move?(input)
+                  puts("Selection must be between 1-9.")
+              elsif board.taken?(input)
+                  puts("Position taken.")
+              else
+                  break;
+              end #if
+          end #loop
+          input
+      end #move
+  end #Human
 end #Module
